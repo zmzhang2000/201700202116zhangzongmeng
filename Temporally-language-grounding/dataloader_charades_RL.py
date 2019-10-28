801c5b9f76f897d9e400e5729231bfef64bd7ae6
zmzhang2000 <929455154@qq.com>
2019-10-28 21:40:01 +0800

    修改dataloader错误，返回输入/目标句子中token的one-hot向量矩阵，返回embedding矩阵

--- Temporally-language-grounding/dataloader_charades_RL.py
+++ Temporally-language-grounding/dataloader_charades_RL.py
@@ -45,7 +45,8 @@ class Charades_Train_dataset(torch.utils.data.Dataset):
          'offset_start_norm': 0.0,
          'proposal_or_sliding_window': 'AO8RW_1_129',
          'sent_skip_thought_vec': [array([[-0.00339404,  0.00549818, -0.00043744, ...,  0.08359446,
-                 0.00238644,  0.01462354]], dtype=float32)],
+                 0.00238644,  0.01462354]], dtype=float32)], 
+         'tokens': ['person', 'is', 'putting', 'book', 'on', 'shelf'] (length <= 10, 去掉a an the . , #)
          'glove_embeddings': numpy.array(shape:[10,300])(单个embedding长300，句子长10，不够用0向量补齐)
          'sentence': 'a person is putting a book on a shelf.',
          'video': 'AO8RW'}
@@ -53,8 +54,21 @@ class Charades_Train_dataset(torch.utils.data.Dataset):
          item['sent_skip_thought_vec']: [numpy.array(shape:[1,4800])]
         '''
 
+        vocab = pickle.load(open(os.path.join(self.data_path, "charades_rl_train_vocab.pkl"), 'rb'))
+        self.vocab, self.embeddings = vocab['words'], vocab['embeddings']
+        '''
+        vocab: 训练集所有词组成的元组
+        embeddings: 第i行对应vocab[i]的embedding  vocab_size x 300
+        '''
+
         self.num_samples_iou = len(self.clip_sentence_pairs_iou_all)
         print((self.num_samples_iou, "iou clip-sentence pairs are readed"))  # 49442
+
+    def word2idx(self, word):
+        if word in self.vocab:
+            return self.vocab.index(word)
+        else:
+            return 2    # 2为<unk>的下标
 
     def read_video_level_feats(self, movie_name, end):
         # read unit level feats by just passing the start and end number
@@ -118,8 +132,19 @@ class Charades_Train_dataset(torch.utils.data.Dataset):
 
         # print(np.shape(global_feature), np.shape(original_feats), np.shape(initial_feature))
 
-        sentence = samples['glove_embeddings']
-        # print(np.shape(sentence))
+        tokens_idx = [self.word2idx(token) for token in samples['tokens']]
+
+        # 将tokens转换为one-hot行向量组成的矩阵 sentence_length x vocab_size
+        input_idx = [0] + tokens_idx        # 输入左填充<sos>
+        sentence_input = np.zeros((self.sentence_size, len(self.vocab)))
+        for i, idx in enumerate(input_idx):
+            sentence_input[i][idx] = 1
+
+        target_idx = tokens_idx + [1]       # 目标右填充<eos>
+        sentence_target = np.zeros((self.sentence_size, len(self.vocab)))
+        for i, idx in enumerate(target_idx):
+            sentence_target[i][idx] = 1
+
         offset_start = samples['offset_start']
         offset_end = samples['offset_end']
         offset_start_norm = samples['offset_start_norm']
@@ -138,7 +163,8 @@ class Charades_Train_dataset(torch.utils.data.Dataset):
         initial_offset_norm[0] = initial_offset_start_norm
         initial_offset_norm[1] = initial_offset_end_norm
 
-        return global_feature, original_feats, initial_feature, sentence, offset_norm, initial_offset, initial_offset_norm, ten_unit, num_units
+        # sentence_input/target * self.embeddings得到sentence_size x 300的句子embedding
+        return global_feature, original_feats, initial_feature, sentence_input, sentence_target, self.embeddings, offset_norm, initial_offset, initial_offset_norm, ten_unit, num_units
 
     def __len__(self):
         return self.num_samples_iou
@@ -176,11 +202,20 @@ class Charades_Test_dataset(torch.utils.data.Dataset):
          'sent_skip_thought_vec': [numpy.array(shape:[1,4800],dtype=float32)],
          'sent_spacy_vec': [numpy.array(shape:[300,],dtype=float32)],
          'sentence': 'person close the door.',
+         'tokens': ['person', 'close', 'door'] (length <= 10, 去掉a an the . , #)
+         'glove_embeddings': numpy.array(shape:[10,300])(单个embedding长300，句子长10，不够用0向量补齐)
          'subj': [],
          'subj_spacy_vec': []}
          
          每个MovieName可能有多个FeatureFileName，每个FeatureFileName可能有多个feature
          每个movie可能有多个clip，每个clip又可能有多个description
+        '''
+
+        vocab = pickle.load(open(os.path.join(self.data_path, "charades_rl_train_vocab.pkl"), 'rb'))
+        self.vocab, self.embeddings = vocab['words'], vocab['embeddings']
+        '''
+        vocab: 训练集所有词组成的元组
+        embeddings: 第i行对应vocab[i]的embedding
         '''
 
         movie_names_set = set()
@@ -202,6 +237,12 @@ class Charades_Test_dataset(torch.utils.data.Dataset):
         0IH69 30.33 757
         ......
         '''
+
+    def word2idx(self, word):
+        if word in self.vocab:
+            return self.vocab.index(word)
+        else:
+            return 2    # 2为<unk>的下标
 
     def read_video_level_feats(self, movie_name, end):
         # read unit level feats by just passing the start and end number
@@ -247,24 +288,30 @@ class Charades_Test_dataset(torch.utils.data.Dataset):
         initial_offset = np.zeros(2, dtype=np.float32)
         initial_offset_norm = np.zeros(2, dtype=np.float32)
         movie_clip_sentences = []
-
         # 以已提取视频特征的最大帧编号作为视频结束点
         checkpoint_paths = glob.glob(os.path.join(self.sliding_clip_path,movie_name + "_*"))
         checkpoint_file_name_ints = [int(float(x.split('/')[-1].split('.npy')[0].split('_')[-1]))
                                      for x in checkpoint_paths]
         end = max(checkpoint_file_name_ints)
-
         global_feature, original_feats, initial_feature, ten_unit, initial_offset_start, initial_offset_end, initial_offset_start_norm, initial_offset_end_norm, num_units \
             = self.read_video_level_feats(movie_name, end)
-
         ten_unit = np.array(ten_unit)
         num_units = np.array(num_units)
 
         # movie_clip_sentences:(FeatureFileName(clip),sent_skip_thought_vec(sentence))
         for dict_2nd in self.clip_sentence_pairs[movie_name]:
             for dict_3rd in self.clip_sentence_pairs[movie_name][dict_2nd]:
-                sentence_vec_ = dict_3rd['glove_embeddings']
-                movie_clip_sentences.append((dict_2nd, sentence_vec_))
+                tokens_idx = [self.word2idx(token) for token in dict_3rd['tokens']]
+                # 将token转换为one-hot向量
+                input_idx = [0] + tokens_idx  # 输入左填充<sos>
+                sentence_input = np.zeros((self.sentence_size, len(self.vocab)))
+                for i, idx in enumerate(input_idx):
+                    sentence_input[i][idx] = 1
+                target_idx = tokens_idx + [1]  # 目标右填充<eos>
+                sentence_target = np.zeros((self.sentence_size, len(self.vocab)))
+                for i, idx in enumerate(target_idx):
+                    sentence_target[i][idx] = 1
+                movie_clip_sentences.append((dict_2nd, sentence_input, sentence_target))
 
         initial_offset[0] = initial_offset_start
         initial_offset[1] = initial_offset_end
@@ -272,4 +319,12 @@ class Charades_Test_dataset(torch.utils.data.Dataset):
         initial_offset_norm[0] = initial_offset_start_norm
         initial_offset_norm[1] = initial_offset_end_norm
 
-        return movie_clip_sentences, global_feature, original_feats, initial_feature, initial_offset, initial_offset_norm, ten_unit, num_units
+        return movie_clip_sentences, self.embeddings, global_feature, original_feats, initial_feature, initial_offset, initial_offset_norm, ten_unit, num_units
+
+
+if __name__ == "__main__":
+    # train = Charades_Train_dataset('E:/Data/charades-features')
+    test = Charades_Test_dataset('E:/Data/charades-features')
+    # a = train.__getitem__(0)
+    b = test.load_movie_slidingclip('001YG')
+    print(b)
